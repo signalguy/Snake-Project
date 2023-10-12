4e9d31e4fe157739f7b128e98202230415ff9f5d
signalguy <sacheol.yang@gmail.com>
2023-10-12 12:27:06 +0900

    second commit

--- scoreboard.py
+++ scoreboard.py
@@ -8,6 +8,9 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        file = open("data.txt")
+        self.high_score = int(file.read())
+        file.close()
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -15,11 +18,20 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score : {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            file = open("data.txt", mode = "w")
+            file.write(str(self.score))
+            file.close()
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
